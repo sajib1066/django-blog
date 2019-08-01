66e714746754217d4e87dda38998bc4553a64a05
sajib1066 <sajib1066@gmail.com>
2019-08-01 13:36:26 +0600

    show featured post in home page

--- webmagblog/views.py
+++ webmagblog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from blog.models import FeaturedPost, Category, Tags, Post
 
 def home_page(request):
-    featured_post = FeaturedPost.objects.all()
+    featured_post = FeaturedPost.objects.featured_post()
     post = Post.objects.all()
     context = {
         'featured_post': featured_post,
