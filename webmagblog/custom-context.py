b5ffe8c62f93872a4a2c5d0298015548ea02d0f1
sajib1066 <sajib1066@gmail.com>
2019-11-21 14:30:44 +0600

    show home menu category

--- webmagblog/custom-context.py
+++ webmagblog/custom-context.py
@@ -3,8 +3,10 @@ from blog.models import Category, Tags
 def category_tag(request):
     category = Category.objects.all()
     tag = Tags.objects.all()
+    menu = Category.objects.all()[:4]
     context = {
         'category': category,
-        'tag': tag
+        'tag': tag,
+        'menu': menu
     }
     return context
