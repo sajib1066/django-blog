66c68a369ef037a5000ffb2de86bd408fca534c4
sajib1066 <sajib1066@gmail.com>
2019-11-21 18:50:35 +0600

    added most read post list

--- webmagblog/views.py
+++ webmagblog/views.py
@@ -6,10 +6,12 @@ def home_page(request):
     header_post = FeaturedPost.objects.featured_post()[:2]
     featured_post = FeaturedPost.objects.featured_post()
     recent_post = Post.objects.filter(is_draft=False).order_by('-id')[:6]
+    most_read_post = Post.objects.filter(is_draft=False).order_by('-id')[:4]
     context = {
         'header_post': header_post,
         'featured_post': featured_post,
-        'recent_post': recent_post
+        'recent_post': recent_post,
+        'most_read_post': most_read_post
     }
     return render(request, 'home.html', context)
 
