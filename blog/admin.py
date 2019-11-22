114bbc6664e640a96ad0b8819e5ce786590c5f15
sajib1066 <sajib1066@gmail.com>
2019-11-22 12:10:19 +0600

    customize admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from .models import Category, Tags, Post, FeaturedPost, MenuCategory
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'title', 'author', 'create_date']
 
-admin.site.register(Category)
-admin.site.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'author', 'create_date']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'author', 'pub_date']
+
+class FeaturedPostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post', 'date']
+
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'menu', 'date']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tags, TagsAdmin)
 admin.site.register(Post, PostAdmin)
-admin.site.register(FeaturedPost)
-admin.site.register(MenuCategory)
+admin.site.register(FeaturedPost, FeaturedPostAdmin)
+admin.site.register(MenuCategory, MenuCategoryAdmin)
