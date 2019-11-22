684cc5edeb3ffd656da47c9e77a641d60b9a3e54
sajib1066 <sajib1066@gmail.com>
2019-11-22 10:24:19 +0600

    added else message

--- webmagblog/views.py
+++ webmagblog/views.py
@@ -18,10 +18,12 @@ def home_page(request):
 def category_page(request, name):
     category_name = Category.objects.get(name=name)
     ctg_feature_post = Post.objects.filter(category=category_name).order_by('-id')[:2]
+    ctg_most_read_post = Post.objects.filter(category=category_name).order_by('-id')[:4]
     post = Post.objects.filter(category=category_name)
     context = {
         'category_name': category_name,
         'ctg_feature_post': ctg_feature_post,
+        'ctg_most_read_post': ctg_most_read_post,
         'post': post
     }
     return render(request, 'category.html', context)
