b2fcf8d715b58c3a9380c01345e25c4a5916b0ef
sajib1066 <sajib1066@gmail.com>
2019-11-24 17:28:06 +0600

    show post in asidebar using custom context

--- webmagblog/custom-context.py
+++ webmagblog/custom-context.py
@@ -1,4 +1,4 @@
-from blog.models import Category, Tags, MenuCategory
+from blog.models import Category, Tags, MenuCategory, Post
 from django.db.models import Count
 
 
@@ -6,10 +6,11 @@ def mycontext(request):
     tag = Tags.objects.all()
     menu = MenuCategory.objects.all()
     category = Category.objects.all().annotate(number_of_posts=Count('post')).order_by('-number_of_posts', 'name')
-
+    recent_post = Post.objects.filter(is_draft=False).order_by('-id')[:3]
     context = {
         'category': category,
         'tag': tag,
-        'menu': menu
+        'menu': menu,
+        'posts': recent_post
     }
     return context
