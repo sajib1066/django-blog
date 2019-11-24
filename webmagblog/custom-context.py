f61309f296e9ac0bea306983be2d3629e2a9c5e8
sajib1066 <sajib1066@gmail.com>
2019-11-24 17:22:36 +0600

    rename context name

--- webmagblog/custom-context.py
+++ webmagblog/custom-context.py
@@ -2,7 +2,7 @@ from blog.models import Category, Tags, MenuCategory
 from django.db.models import Count
 
 
-def category_tag(request):
+def mycontext(request):
     tag = Tags.objects.all()
     menu = MenuCategory.objects.all()
     category = Category.objects.all().annotate(number_of_posts=Count('post')).order_by('-number_of_posts', 'name')
--- webmagblog/settings.py
+++ webmagblog/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'webmagblog.custom-context.category_tag',
+                'webmagblog.custom-context.mycontext',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
