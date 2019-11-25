a38a967077e159c94c826c12671de67735c6553c
sajib1066 <sajib1066@gmail.com>
2019-11-25 12:47:48 +0600

    added search feature

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import category, blog_post
+from .views import category, blog_post, search_post
 
 urlpatterns = [
     path('category', category, name='category'),
-    path('post/<post>', blog_post, name='blog-post')
+    path('post/<post>', blog_post, name='blog-post'),
+    path('search', search_post, name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from .models import Post
+
 # Create your views here.
 def category(request):
     return render(request, 'blog/category.html')
@@ -12,3 +12,13 @@ def blog_post(request, post):
         return render(request, 'blog/blog-post.html', context)
     except:
         return render(request, '404.html')
+
+def search_post(request):
+    if request.method == 'POST':
+        data = request.POST['search']
+        posts = Post.objects.filter(title__icontains=data)
+        context = {
+            'posts': posts
+        }
+        return render(request, 'blog/search.html', context)
+    return render(request, 'blog/search.html')
