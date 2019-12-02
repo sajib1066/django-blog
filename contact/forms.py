f9baf97d2c9cfbf8d155fb0ad191bbae406e9380
sajib1066 <sajib1066@gmail.com>
2019-12-02 22:30:59 +0600

    update forms

--- contact/forms.py
+++ contact/forms.py
@@ -6,8 +6,8 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
-            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}),
-            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message'})
+            'name': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Your Name'}),
+            'email': forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Your Email'}),
+            'subject': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Subject'}),
+            'message': forms.Textarea(attrs={'class': 'input', 'placeholder': 'Message'})
         }
