132bb4291e5417a583a705309e041995233a00e4
sajib1066 <sajib1066@gmail.com>
2019-12-10 19:00:27 +0600

    create newsletter form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Contact
+from .models import Contact, NewsLetter
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -10,4 +10,12 @@ class ContactForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Your Email'}),
             'subject': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Subject'}),
             'message': forms.Textarea(attrs={'class': 'input', 'placeholder': 'Message'})
+        }
+
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = NewsLetter
+        fields = ('email', )
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Enter your email'})
         }
